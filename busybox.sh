482dc0e285d1d9a5355f2d5e83405209b86f16e9
ReBullet <rebullet@ik.me>
2025-09-19 12:05:14 +0400

    Update busybox.sh

--- busybox.sh
+++ busybox.sh
@@ -12,7 +12,7 @@ TERMUX_INCLUDE="${TERMUX_PREFIX}/include"
 TERMUX_TMPDIR="${TERMUX_HOME}/tmp"
 TERMUX_CACHEDIR="${TERMUX_HOME}/cache"
 
-mkdir -p "$TERMUX_TMPDIR" "$TERMUX_CACHEDIR" "$TERMUX_BIN" "$TERMUX_LIB" "$TERMUX_LIB32" "$TERMUX_ETC" "$TERMUX_INCLUDE"
+mkdir -p "$TERMUX_TMPDIR" "$TERMUX_CACHEDIR" "$TERMUX_BIN" "$TERMUX_LIB" "$TERMUX_LIB32" "$TERMUX_LIB/pkgconfig" "$TERMUX_ETC" "$TERMUX_INCLUDE"
 
 termux_download() {
     local url="$1"
@@ -22,7 +22,6 @@ termux_download() {
         exit 1
     fi
     if [ ! -f "$output" ]; then
-        echo "Downloading $url..."
         wget --tries=3 --show-progress -qO "$output" "$url" || {
             echo "Failed to download $url"
             exit 1
@@ -33,9 +32,8 @@ termux_download() {
 termux_extract() {
     local tarfile="$1"
     local dirname="$2"
-    echo "Extracting $tarfile..."
     mkdir -p "$TERMUX_TMPDIR/$dirname"
-    tar -xf "$tarfile" -C "$TERMUX_TMPDIR" || {
+    tar -xf "$tarfile" -C "$TERMUX_TMPDIR/$dirname" --strip-components=1 || {
         echo "Failed to extract $tarfile"
         exit 1
     }
@@ -43,81 +41,62 @@ termux_extract() {
 
 BUSYBOX_VERSION="1.37.0"
 BUSYBOX_SRCURL="https://busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2"
-BUSYBOX_PATCHES=(
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0000-use-clang.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0001-clang-fix.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0002-hardcoded-paths-fix.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0003-strchrnul-fix.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0005-no-change-identity.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0006-miscutils-crond.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0007-miscutils-crontab.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0008-networking-ftpd-no-chroot.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0009-networking-httpd-default-port.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0011-networking-tftp-no-chroot.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0012-util-linux-mount-no-addmntent.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0013-busybox-1.36.1-kernel-6.8.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0014-fix-segfault.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0015-fix-ipv6.patch"
-    "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0016-fix-ipv6-2.patch"
-)
-BUSYBOX_CONFIG_URL="https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/busybox.config"
+BUSYBOX_SHA256="3311dff32e746499f4df0d5df04d7eb396382d7e108bb9250e7b519b837043a4"
 
 termux_build_busybox() {
-    echo "Building busybox..."
     cd "$TERMUX_TMPDIR/busybox-$BUSYBOX_VERSION"
-    sed -i '168i\
-#ifndef __has_include\n\
-#define __has_include(x) 0\n\
-#endif\n\
-#if __has_include(<byteswap.h>)\n\
-# include <byteswap.h>\n\
-#else\n\
-# define bswap_16(x) __builtin_bswap16(x)\n\
-# define bswap_32(x) __builtin_bswap32(x)\n\
-# define bswap_64(x) __builtin_bswap64(x)\n\
-#endif' include/platform.h
-    for patch_url in "${BUSYBOX_PATCHES[@]}"; do
-        patch_name=$(basename "$patch_url")
-        termux_download "$patch_url" "$TERMUX_CACHEDIR/$patch_name"
-        echo "Applying patch $patch_name..."
-        patch -p1 < "$TERMUX_CACHEDIR/$patch_name" || {
-            echo "Failed to apply patch $patch_name"
-            exit 1
-        }
+    for patch in "$TERMUX_CACHEDIR"/00*.patch; do
+        patch -p1 < "$patch" || { echo "Failed to apply $patch"; exit 1; }
     done
-    termux_download "$BUSYBOX_CONFIG_URL" "$TERMUX_CACHEDIR/busybox.config"
-    sed -e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
-        -e "s|@TERMUX_SYSROOT@|$TERMUX_HOME/usr|g" \
-        -e "s|@TERMUX_HOST_PLATFORM@|${TERMUX_HOST_PLATFORM:-aarch64-linux-android}|g" \
-        -e "s|@TERMUX_CFLAGS@|-Wno-ignored-optimization-argument -Wno-unused-command-line-argument -I$TERMUX_HOME/usr/include|g" \
-        -e "s|@TERMUX_LDFLAGS@||g" \
+    CFLAGS+=" -Wno-ignored-optimization-argument -Wno-unused-command-line-argument"
+    sed -e "s|@TERMUX_PREFIX@|/system_ext|g" \
+        -e "s|@TERMUX_SYSROOT@||g" \
+        -e "s|@TERMUX_HOST_PLATFORM@|aarch64-linux-android|g" \
+        -e "s|@TERMUX_CFLAGS@|$CFLAGS|g" \
+        -e "s|@TERMUX_LDFLAGS@|$LDFLAGS|g" \
         -e "s|@TERMUX_LDLIBS@|log|g" \
         "$TERMUX_CACHEDIR/busybox.config" > .config
+    unset CFLAGS LDFLAGS
     make oldconfig
-    export CFLAGS="-I$TERMUX_INCLUDE -I$TERMUX_HOME/usr/include"
     make -j$(nproc)
-    install -Dm700 "./0_lib/busybox_unstripped" "$TERMUX_BIN/busybox"
-    install -Dm700 "./0_lib/libbusybox.so.${BUSYBOX_VERSION}_unstripped" "$TERMUX_LIB/libbusybox.so.${BUSYBOX_VERSION}"
-    ln -sf "$TERMUX_LIB/libbusybox.so.${BUSYBOX_VERSION}" "$TERMUX_LIB/libbusybox.so"
-    install -Dm600 -t "$TERMUX_ETC/man/man1" "docs/busybox.1"
-    ln -sf "$TERMUX_BIN/busybox" "$TERMUX_BIN/ash"
-    ln -sf "$TERMUX_ETC/man/man1/busybox.1" "$TERMUX_ETC/man/man1/ash.1"
-    mkdir -p "$TERMUX_LIBexec/busybox"
+    INSTALL_DIR="$TERMUX_PREFIX/system_ext"
+    mkdir -p "$INSTALL_DIR/bin" "$INSTALL_DIR/lib" "$INSTALL_DIR/share/man/man1" "$INSTALL_DIR/libexec/busybox"
+    install -Dm700 "./0_lib/busybox_unstripped" "$INSTALL_DIR/bin/busybox"
+    install -Dm700 "./0_lib/libbusybox.so.${BUSYBOX_VERSION}_unstripped" "$INSTALL_DIR/lib/libbusybox.so.${BUSYBOX_VERSION}"
+    ln -sfr "$INSTALL_DIR/lib/libbusybox.so.${BUSYBOX_VERSION}" "$INSTALL_DIR/lib/libbusybox.so"
+    install -Dm600 -t "$INSTALL_DIR/share/man/man1" "docs/busybox.1"
+    ln -sfr "$INSTALL_DIR/bin/busybox" "$INSTALL_DIR/bin/ash"
+    ln -sfr "$INSTALL_DIR/share/man/man1/busybox.1" "$INSTALL_DIR/share/man/man1/ash.1"
+    local applet
     for applet in 'less' 'nc' 'vi'; do
-        cat > "$TERMUX_LIBexec/busybox/$applet" <<EOF
-#!/bin/sh
-exec busybox $applet "\$@"
-EOF
-        chmod 700 "$TERMUX_LIBexec/busybox/$applet"
+        {
+            echo "#!/system_ext/bin/sh"
+            echo "exec busybox $applet \"\$@\""
+        } > "$INSTALL_DIR/libexec/busybox/$applet"
+        chmod 700 "$INSTALL_DIR/libexec/busybox/$applet"
     done
 }
 
 main() {
-    echo "Installing build dependencies..."
     pkg_install="pkg_install_$(uname -m)"
-    $pkg_install build-essential wget tar patch bzip2 binutils
-    echo "Downloading sources..."
+    $pkg_install build-essential wget tar patch xz-utils bzip2 binutils
     termux_download "$BUSYBOX_SRCURL" "$TERMUX_CACHEDIR/busybox-$BUSYBOX_VERSION.tar.bz2"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0000-use-clang.patch" "$TERMUX_CACHEDIR/0000-use-clang.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0001-clang-fix.patch" "$TERMUX_CACHEDIR/0001-clang-fix.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0002-hardcoded-paths-fix.patch" "$TERMUX_CACHEDIR/0002-hardcoded-paths-fix.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0003-strchrnul-fix.patch" "$TERMUX_CACHEDIR/0003-strchrnul-fix.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0005-no-change-identity.patch" "$TERMUX_CACHEDIR/0005-no-change-identity.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0006-miscutils-crond.patch" "$TERMUX_CACHEDIR/0006-miscutils-crond.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0007-miscutils-crontab.patch" "$TERMUX_CACHEDIR/0007-miscutils-crontab.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0008-networking-ftpd-no-chroot.patch" "$TERMUX_CACHEDIR/0008-networking-ftpd-no-chroot.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0009-networking-httpd-default-port.patch" "$TERMUX_CACHEDIR/0009-networking-httpd-default-port.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0011-networking-tftp-no-chroot.patch" "$TERMUX_CACHEDIR/0011-networking-tftp-no-chroot.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0012-util-linux-mount-no-addmntent.patch" "$TERMUX_CACHEDIR/0012-util-linux-mount-no-addmntent.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0013-busybox-1.36.1-kernel-6.8.patch" "$TERMUX_CACHEDIR/0013-busybox-1.36.1-kernel-6.8.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0014-fix-segfault.patch" "$TERMUX_CACHEDIR/0014-fix-segfault.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0015-fix-ipv6.patch" "$TERMUX_CACHEDIR/0015-fix-ipv6.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/0016-fix-ipv6-2.patch" "$TERMUX_CACHEDIR/0016-fix-ipv6-2.patch"
+    termux_download "https://raw.githubusercontent.com/termux/termux-packages/refs/heads/master/packages/busybox/busybox.config" "$TERMUX_CACHEDIR/busybox.config"
     termux_extract "$TERMUX_CACHEDIR/busybox-$BUSYBOX_VERSION.tar.bz2" "busybox-$BUSYBOX_VERSION"
     termux_build_busybox
     echo "- Binaries: $TERMUX_BIN"
@@ -125,13 +104,14 @@ main() {
     echo "- Libraries (32-bit): $TERMUX_LIB32"
     echo "- Configs: $TERMUX_ETC"
     echo "- Headers: $TERMUX_INCLUDE"
+    echo "Built files in $TERMUX_PREFIX/system_ext"
 }
 
 pkg_install_aarch64() {
     pkg_install_arm64
 }
 pkg_install_arm64() {
-    pkg update -y && pkg install -y build-essential wget tar patch bzip2 binutils
+    pkg update -y && pkg install -y build-essential wget tar patch xz-utils bzip2 binutils
 }
 
 main
